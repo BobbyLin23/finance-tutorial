ae6e0da7791f70d1ab6851edfacb6369ca0c193a
BobbyLin23 <linzhangsheng23@gmail.com>
2024-05-21 23:27:46 +0800

    feat: add accounts edit and delete

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,10 +1,10 @@
+import { z } from 'zod'
 import { Hono } from 'hono'
 import { HTTPException } from 'hono/http-exception'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
-import { and, eq, inArray } from 'drizzle-orm'
 import { zValidator } from '@hono/zod-validator'
+import { and, eq, inArray } from 'drizzle-orm'
 import { createId } from '@paralleldrive/cuid2'
-import { z } from 'zod'
 
 import { db } from '@/db/drizzle'
 import { accounts, insertAccountSchema } from '@/db/schema'
@@ -29,6 +29,46 @@ const app = new Hono()
 
     return c.json({ data })
   })
+  .get(
+    '/:id',
+    zValidator(
+      'param',
+      z.object({
+        id: z.string().optional(),
+      }),
+    ),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c)
+      const { id } = c.req.valid('param')
+
+      if (!id) {
+        throw new HTTPException(400, {
+          res: c.json({ error: 'Invalid id' }, 400),
+        })
+      }
+
+      if (!auth?.userId) {
+        throw new HTTPException(401, {
+          res: c.json({ error: 'Unauthorized' }, 401),
+        })
+      }
+
+      const [data] = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(and(eq(accounts.user_id, auth.userId), eq(accounts.id, id)))
+
+      if (!data) {
+        return c.json({ error: 'Not found' }, 404)
+      }
+
+      return c.json({ data })
+    },
+  )
   .post(
     '/',
     clerkMiddleware(),
@@ -83,6 +123,72 @@ const app = new Hono()
         .returning({
           id: accounts.id,
         })
+
+      return c.json({ data })
+    },
+  )
+  .patch(
+    '/:id',
+    clerkMiddleware(),
+    zValidator('param', z.object({ id: z.string().optional() })),
+    zValidator(
+      'json',
+      insertAccountSchema.pick({
+        name: true,
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c)
+      const { id } = c.req.valid('param')
+      const values = c.req.valid('json')
+
+      if (!id) {
+        return c.json({ error: 'Invalid id' }, 400)
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+
+      const [data] = await db
+        .update(accounts)
+        .set(values)
+        .where(and(eq(accounts.user_id, auth.userId), eq(accounts.id, id)))
+        .returning()
+
+      if (!data) {
+        return c.json({ error: 'Not found' }, 404)
+      }
+
+      return c.json({ data })
+    },
+  )
+  .delete(
+    '/:id',
+    clerkMiddleware(),
+    zValidator('param', z.object({ id: z.string().optional() })),
+    async (c) => {
+      const auth = getAuth(c)
+      const { id } = c.req.valid('param')
+
+      if (!id) {
+        return c.json({ error: 'Invalid id' }, 400)
+      }
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+
+      const [data] = await db
+        .delete(accounts)
+        .where(and(eq(accounts.user_id, auth.userId), eq(accounts.id, id)))
+        .returning({
+          id: accounts.id,
+        })
+
+      if (!data) {
+        return c.json({ error: 'Not found' }, 404)
+      }
 
       return c.json({ data })
     },
--- features/accounts/components/account-form.tsx
+++ features/accounts/components/account-form.tsx
@@ -47,7 +47,6 @@ export const AccountForm = ({
   })
 
   const handleDelete = () => {
-    console.log('delete')
     onDelete?.()
   }
 
