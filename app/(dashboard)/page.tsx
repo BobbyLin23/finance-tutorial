694c6ab6f57f035bc3a82d915bcf9bde8a50dc42
BobbyLin23 <linzhangsheng23@gmail.com>
2024-06-05 22:20:09 +0800

    feat: add dashboard chart

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
 import { DataGrid } from '@/components/data-grid'
+import { DataCharts } from '@/components/data-charts'
 
 export default function Home() {
   return (
     <main className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
       <DataGrid />
+      <DataCharts />
     </main>
   )
 }
